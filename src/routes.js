9ec36a4df12d368ff1bbe492c78f0017b8fd8152
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-06-01 15:29:13 +0700

    handle image req

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,16 @@ const routes = [
     path: '/fakultas',
     handler: getAllFakultasHandler,
   },
+  {
+    method: 'GET',
+    path: '/img/{image*}',
+    handler: {
+      directory: {
+        path: 'public/img',
+        listing: true,
+      },
+    },
+  },
 ]
 
 module.exports = routes
