c89e9909edf6770469f1b7badb53f097aa446327
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-06-01 14:23:57 +0700

    Create fakultas.js

---
+++ src/fakultas.js
@@ -0,0 +1,63 @@
+const fakultas = [
+  {
+    name: 'FPMIPA',
+    url_image: 'FPMIPA.jpg',
+    full_name: 'Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam',
+    gallery: [
+      'FPMIPA.jpg',
+      'FPMIPA.jpg',
+      'FPMIPA.jpg',
+      'FPMIPA.jpg',
+      'FPMIPA.jpg',
+      'FPMIPA.jpg',
+    ],
+    ratio: '1:30',
+    avg_study_time: '3.5',
+    prodi: [
+      {
+        name: 'Pendidikan Matematika',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'FPMIPA.jpg',
+        slug: 's1-pendidikan-matematika',
+      },
+      {
+        name: 'Pendidikan Biologi',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'FPMIPA.jpg',
+        slug: 's1-pendidikan-biologi',
+      },
+      {
+        name: 'Pendidikan Kimia',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'FPMIPA.jpg',
+        slug: 's1-pendidikan-kimia',
+      },
+      {
+        name: 'Pendidikan Fisika',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'FPMIPA.jpg',
+        slug: 's1-pendidikan-fisika',
+      },
+      {
+        name: 'Pendidikan Ilmu Komputer',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'FPMIPA.jpg',
+        slug: 's1-pendidikan-ilmu-komputer',
+      },
+      {
+        name: 'International Program on Science Education',
+        jenjang: 'S1',
+        akreditasi: 'A',
+        url_image: 'FPMIPA.jpg',
+        slug: 's1-international-program-on-science-education',
+      },
+    ],
+  },
+]
+
+module.exports = fakultas
