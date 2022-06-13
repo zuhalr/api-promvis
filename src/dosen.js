1acbc175052957aae64eb8448e578e86448eb651
HENDI YAHYA <yahyahendi35@gmail.com>
2022-06-13 15:30:28 +0700

    Modify Dosen API

--- src/dosen.js
+++ src/dosen.js
@@ -3,18 +3,39 @@ const baseImagePath = './public/images/dosen/'
 
 const dosen = [
   {
-    slug: 'fpmipa',
-    name: 'FPMIPA',
-    url_image: 'http://localhost:3000/img/FPMIPA.jpg',
-    full_name: 'Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam',
-    jml_mahasiswa: 1500,
-    jml_dosen: 200,
-    jml_prodi: 9,
-    wkt_lulus:'4.5 Tahun',
-    kerjasama:' - 2020 UN\n - 2021 UM',
-    fasilitas: ' - Kantin\n - Student Corner',
-    ratio: '1:7.5',
-    avg_study_time: '3.5',
+    nama: "Rasim",
+    jabatan: "lektor",
+    riwayat_pendidikan:[
+      {
+        univ: "STMIK IM",
+        jenjang :"S1"
+      },{
+        univ: "ITB",
+        jenjang :"S2"
+      },{
+        univ: "ITB",
+        jenjang :"S3"
+      },
+    ],
+    riwayat_pengajaran:[
+      {
+      matkul : "nama matkul",
+      semester : "Genap 2004"
+      },{
+        matkul : "nama matkul",
+        semester : "Genap 2004"
+      }
+    ],
+    riwayat_prestasi:[
+      {
+        nama: "nama prestasi",
+        tahun: "tahun prestasi"
+      },
+      {
+        nama: "nama prestasi",
+        tahun: "tahun prestasi"
+      },
+    ]
   },
 ]
 
