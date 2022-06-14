7b02aa1b47cfc043d3dfa00b07e9da3d73a5f2f1
HENDI YAHYA <yahyahendi35@gmail.com>
2022-06-14 11:59:15 +0700

    Modify Facility

--- src/fasilitas.js
+++ src/fasilitas.js
@@ -4,52 +4,52 @@ const baseImagePath = './public/images/fasilitas/'
 const fasilitas  = [
     {
       name: 'Student Corner', 
-      url_image: 'http://localhost:3000/img/FIP.png', 
+      url_image: 'http://localhost:3000/img/fasilitas/sc.png', 
       fakultas: 'fpmipa',
     },
     {
-        name: 'Kantin', 
-        url_image: 'http://localhost:3000/img/FIP.png', 
+        name: 'Auditorium', 
+        url_image: 'http://localhost:3000/img/fasilitas/audit-jica.jpeg', 
         fakultas: 'fpmipa',
     }, 
     {
       name: 'Lab. Komputer', 
-      url_image: 'http://localhost:3000/img/FIP.png', 
+      url_image: 'http://localhost:3000/img/fasilitas/Lab-Komputer.png', 
       fakultas: 'fip',
     }, 
     {
       name: 'Kantin', 
-      url_image: 'http://localhost:3000/img/FIP.png', 
+      url_image: 'http://localhost:3000/img/fasilitas/kantin.jpg', 
       fakultas: 'fip',
     },
     {
-      name: 'Audiotorium', 
-      url_image: 'http://localhost:3000/img/FIP.png', 
+      name: 'Auditorium', 
+      url_image: 'http://localhost:3000/img/fasilitas/Auditorium.png', 
       fakultas: 'fpips',
     }, 
     {
       name: 'Lab. IPS', 
-      url_image: 'http://localhost:3000/img/FIP.png', 
+      url_image: 'http://localhost:3000/img/fasilitas/Lab-Komputer.png',                                                        
       fakultas: 'fpips',
     },
     {
       name: 'Ruang Kesenian', 
-      url_image: 'http://localhost:3000/img/FIP.png', 
+      url_image: 'http://localhost:3000/img/fasilitas/kesenian.jpeg',   
       fakultas: 'fpsd',
     }, 
     {
-      name: 'Ruang Pagelaran', 
-      url_image: 'http://localhost:3000/img/FIP.png', 
+      name: 'Amphitheater', 
+      url_image: 'http://localhost:3000/img/fasilitas/amphiteater.jpg', 
       fakultas: 'fpsd',
     },
     {
       name: 'Gelanggang', 
-      url_image: 'http://localhost:3000/img/FIP.png', 
+      url_image: 'http://localhost:3000/img/fasilitas/kolam.jpg', 
       fakultas: 'fptk',
     },   
     {
       name: 'Stadium Olahraga', 
-      url_image: 'http://localhost:3000/img/FIP.png', 
+      url_image: 'http://localhost:3000/img/fasilitas/stadion.jpg', 
       fakultas: 'fptk',
     }, 
   ]  
